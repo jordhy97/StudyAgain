83d12f32fe9774163e47d9a973321424c533c275
Jordhy Fernando <jordhy.fernando@gmail.com>
2017-07-19 20:29:34 +0700

    Set up API and web route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+#Authentication
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+Route::get('user', 'AuthController@userInfo');
+
+#Question
+Route::get('questions', 'QuestionController@index');
+Route::get('questions/{id}', 'QuestionController@show');
+Route::post('questions', 'QuestionController@store');
+Route::put('questions/{id}', 'QuestionController@update');
+Route::delete('questions/{id}', 'QuestionController@destroy');
+
+#Answer
+Route::post('questions/{id}/answers', 'AnswerController@store');
+Route::put('answers/{id}', 'AnswerController@update');
+Route::delete('answers/{id}', 'AnswerController@destroy');
+
